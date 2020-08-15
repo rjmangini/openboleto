2c6bcb8bbd4178623c3a7b14ae4a352c026c94aa
Rodrigo Mangini <rjmangini@gmail.com>
2020-08-15 14:25:06 -0300

    Ajustes linha digitavel Santander

--- samples/santander.php
+++ samples/santander.php
@@ -6,57 +6,56 @@ use OpenBoleto\Banco\Santander;
 use OpenBoleto\Agente;
 
 $sacado = new Agente('Fernando Maia', '023.434.234-34', 'ABC 302 Bloco N', '72000-000', 'Brasília', 'DF');
-$cedente = new Agente('Empresa de cosméticos LTDA', '02.123.123/0001-11', 'CLS 403 Lj 23', '71000-000', 'Brasília', 'DF');
+$cedente = new Agente('Empresa de cosméticos LTDA', '18361518000116', 'CLS 403 Lj 23', '71000-000', 'Brasília', 'DF');
 
-$boleto = new Santander(array(
-    // Parâmetros obrigatórios
-    'dataVencimento' => new DateTime('2020-09-07'),
+$boleto = [
+    'vencimento' => '2020-09-07',
     'valor' => 1444.2,
-    'sequencial' => 57252, // Até 13 dígitos
+    'seq_reg_remessa' => 19,
+    'agencia' => '4254',
+    'agencia_dv' => null,
+    'numero' => '13002540',
+    'numero_dv' => '6',
+    'num_carteira' => 5,
+    'documento' => '27200-1/1',
+    'uso_empresa' => '57252',
+    'aceite' => 'N',
+    'uso_banco' => '4705670',
+    'especie' => '01',
+];
+$multaDiaria = 1.23;
+$multaAtraso = 12.34;
+$nfe = '27200';
+
+$boletoPdf = new Santander(array(
+    // Parâmetros obrigatórios
+    'dataVencimento' => new \DateTime($boleto['vencimento']),
+    'valor' => $boleto['valor'],
+    'sequencial' => $boleto['uso_empresa'],
     'sacado' => $sacado,
     'cedente' => $cedente,
-    'agencia' => 4254, // Até 4 dígitos
-    'carteira' => 5, // 101, 102 ou 201
-    'conta' => 13002540, // Código do cedente: Até 7 dígitos
-     // IOS – Seguradoras (Se 7% informar 7. Limitado a 9%)
-     // Demais clientes usar 0 (zero)
-    'ios' => '0', // Apenas para o Santander
-
-    // Parâmetros recomendáveis
-    //'logoPath' => 'http://empresa.com.br/logo.jpg', // Logo da sua empresa
-    'contaDv' => 2,
-    'agenciaDv' => 1,
-    'carteiraDv' => 1,
+    'agencia' => $boleto['agencia'],
+    'agenciaDv' => $boleto['agencia_dv'],
+    'conta' => $boleto['uso_banco'],
+    //'contaDv' => $boleto['numero_dv'],
+    'carteira' => $boleto['num_carteira'],
+    'numeroDocumento' => $boleto['documento'],
     'descricaoDemonstrativo' => array( // Até 5
-        'Compra de materiais cosméticos',
-        'Compra de alicate',
     ),
     'instrucoes' => array( // Até 8
-        'Após o dia 30/11 cobrar 2% de mora e 1% de juros ao dia.',
-        'Não receber após o vencimento.',
+        'APOS VENCIMENTO COBRAR R$ ' . $multaDiaria . ' POR DIA DE ATRASO',
+        'APOS VENCIMENTO COBRAR MULTA DE R$ ' . $multaAtraso,
+        'PRODUTOS "DYSTRAY"',
+        'NUMERO INTERNO: ' . $boleto['uso_empresa'] . ' / DANFE: ' . $nfe,
+        'APOS 5 DIAS DO VENCIMENTO O TITULO SERA PROTESTADO / NEGATIVADO',
+        'COBRANCA (11) 5510-5050 - WHATSAPP (11) 93293-8660',
     ),
-
-    // Parâmetros opcionais
-    //'resourcePath' => '../resources',
-    //'moeda' => Santander::MOEDA_REAL,
-    //'dataDocumento' => new DateTime(),
-    //'dataProcessamento' => new DateTime(),
-    //'contraApresentacao' => true,
-    //'pagamentoMinimo' => 23.00,
-    //'aceite' => 'N',
-    'especieDoc' => 'AB01C',
-    'numeroDocumento' => '27200-1/1',
-    'usoBanco' => '4705670',
-    //'layout' => 'layout.phtml',
-    //'logoPath' => 'http://boletophp.com.br/img/opensource-55x48-t.png',
-    //'sacadorAvalista' => new Agente('Antônio da Silva', '02.123.123/0001-11'),
-    //'descontosAbatimentos' => 123.12,
-    //'moraMulta' => 123.12,
-    //'outrasDeducoes' => 123.12,
-    //'outrosAcrescimos' => 123.12,
-    //'valorCobrado' => 123.12,
-    //'valorUnitario' => 123.12,
-    //'quantidade' => 1,
+    'moeda' => Santander::MOEDA_REAL,
+    'dataDocumento' => new \DateTime(),
+    'dataProcessamento' => new \DateTime(),
+    'aceite' => $boleto['aceite'],
+    'especieDoc' => $boleto['especie'],
+    'usoBanco' => $boleto['uso_banco'],
 ));
 
-echo $boleto->getOutput();
+echo $boletoPdf->getOutput();
